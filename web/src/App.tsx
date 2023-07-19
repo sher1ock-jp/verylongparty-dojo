5191ef6ab22144539898ea027a61c2751331afa1
Takayuki Ono <takayukiono@Takayukis-MacBook-Air.local>
2023-07-19 14:19:37 +0200

    done wallet connect only

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,46 +1,109 @@
+import { useState } from 'react';
 import './App.css';
-import {useDojo} from './DojoContext';
-import {useComponentValue} from "@dojoengine/react";
-import {Direction} from './dojo/createSystemCalls'
-import {Utils} from '@dojoengine/core';
-import { Provider, Account, ec } from "starknet";
+import Square from './component/square';
+import { useStoreWallet } from './component/wallet/walletContext';
+import { encode } from "starknet";
+import { StarknetWindowObject, connect } from "get-wallet-starknet";
+import { ChakraProvider } from '@chakra-ui/react'
+import { Center,Button } from '@chakra-ui/react';
 
-function App() {
-    const {
-        systemCalls: {spawn, move},
-        components: {Moves, Position},
-    } = useDojo();
-
-    const entityId = BigInt(import.meta.env.VITE_ENTITY_ID);
-    const position = useComponentValue(Position, Utils.getEntityIdFromKeys([entityId]));
-    const moves = useComponentValue(Moves, Utils.getEntityIdFromKeys([entityId]));
-
-
-    return (
-        <>
-            <div className="card">
-                <button onClick={() => spawn()}>Spawn</button>
-            </div>
-            <div className="card">
-                <button onClick={() => move(Direction.Left)}>Move Left</button>
-            </div>
-            <div className="card">
-                <button onClick={() => move(Direction.Right)}>Move Right</button>
-            </div>
-            <div className="card">
-                <button onClick={() => move(Direction.Up)}>Move Up</button>
-            </div>
-            <div className="card">
-                <button onClick={() => move(Direction.Down)}>Move Down</button>
-            </div>
-            <div className="card">
-                <div>Moves Remaining: {moves ? `${moves['remaining']}` : 'Need to Spawn'}</div>
-            </div>
-            <div className="card">
-                <div>Position: {position ? `${position['x']}, ${position['y']}` : 'Need to Spawn'}</div>
-            </div>
-        </>
-    );
+interface Square {
+  id: number;
+  x: number;
+  y: number;
 }
 
+const App = () => {
+  const [initialFocusId] = useState(1275);
+
+  const gridSize = 50;
+  const centerX = Math.floor(gridSize / 2);
+  const centerY = Math.floor(gridSize / 2);
+
+  const squares: Square[] = [];
+  for (let id = 1; id < 2501; id++) {
+    const x = id % gridSize - centerX;
+    const y = Math.floor(id / gridSize) - centerY;
+    squares.push({ id, x, y });
+  }
+
+  // Connect Argent-X or Braavos wallet
+  const [isConnected, setConnected] = useState(false);
+  const [wallet, setWallet] = useState<StarknetWindowObject | null>(null);
+  const addressAccountFromContext = useStoreWallet(state => state.address);
+  const setAddressAccount = useStoreWallet(state => state.setAddressAccount);
+  const chainFromContext = useStoreWallet(state => state.chain);
+  const setChain = useStoreWallet(state => state.setChain);
+  const accountFromContext = useStoreWallet(state => state.account);
+  const setAccount = useStoreWallet(state => state.setAccount);
+  const providerFromContext = useStoreWallet(state => state.provider);
+  const setProvider = useStoreWallet(state => state.setProvider);
+
+  // Component context
+
+  const handleConnectClick = async () => {
+      const wallet = await connect({ modalMode: "alwaysAsk", modalTheme: "light" });
+      await wallet?.enable({ starknetVersion: "v5" } as any);
+      setWallet(wallet);
+      const addr = encode.addHexPrefix(encode.removeHexPrefix(wallet?.selectedAddress ?? "0x").padStart(64, "0"));
+      setAddressAccount(addr);
+      setConnected(!!wallet?.isConnected);
+      if (wallet?.account) {
+          setAccount(wallet.account);
+      }
+      if (wallet?.isConnected) {
+          setChain(wallet.chainId); // not provided by Braavos
+          setProvider(wallet.provider);
+      }
+  }
+
+  return (
+    <body>
+      <div className="background">
+        <div className="square-zone">
+          {squares.map((square) => (
+            <Square
+                key={square.id}
+                squareId={square.id}
+                coordinateX={square.x}
+                coordinateY={square.y}
+                initialFocusId={initialFocusId}
+            />
+            ))}
+
+            {wallet ? (
+            <>
+            </>
+            ) : (
+            <>
+                <div className="wallet-area">
+                    <Center>
+                        <Button
+                            ml="4"
+                            textDecoration="none !important"
+                            outline="none !important"
+                            boxShadow="none !important"
+                            onClick={() => {
+                                handleConnectClick();
+                            }}
+                        >
+                            Connect Wallet
+                        </Button>
+                    </Center>
+                </div>
+            </>
+            )}
+
+        </div>
+      </div>
+    </body>
+  );
+};
+
 export default App;
+
+
+
+
+
+
