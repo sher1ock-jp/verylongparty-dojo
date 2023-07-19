b61232befdb388e3141530e7445c4d4781d92236
Takayuki Ono <takayukiono@Takayukis-MacBook-Air.local>
2023-07-19 11:19:27 +0200

    w/o array for check new, return all images to front

--- web/src/dojo/createSystemCalls.ts
+++ web/src/dojo/createSystemCalls.ts
@@ -2,33 +2,26 @@ import { SetupNetworkResult } from "./setupNetwork";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
-export enum Direction {
-    Left = 0,
-    Right = 1,
-    Up = 2,
-    Down = 3,
-}
-
 export function createSystemCalls(
     { execute, syncWorker }: SetupNetworkResult,
 ) {
-    const spawn = async () => {
-        const tx = await execute("spawn", []);
+    const gameStart = async () => {
+        const tx = await execute("GameStart", []);
         // await awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
         syncWorker.sync(tx.transaction_hash);
-        
+        console.log("GameStart", )
     };
 
-    const move = async (direction: Direction) => {
+    const squareCreate = async () => {
         // execute from core
-        const tx = await execute("move", [direction]);
+        const tx = await execute("SquareCreate", []);
         // awaitStreamValue(txReduced$, (txHash) => txHash === tx.transaction_hash);
         syncWorker.sync(tx.transaction_hash);
-        console.log("Moved ", direction)
+        console.log("SquareCreate", )
       };
 
     return {
-        spawn,
-        move
+        gameStart,
+        squareCreate,
     };
 }
